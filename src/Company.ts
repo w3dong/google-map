3c791d4c70a397dfde32c53df8188b1fcf0d96f8
w3dong <w3dong@gmail.com>
2022-09-01 23:07:19 -0700

    first commit

---
+++ src/Company.ts
@@ -0,0 +1,25 @@
+import faker from "faker";
+import { Mappable } from "./CustomMap";
+export class Company implements Mappable{
+    companyName: string;
+    catchPrase: string;
+    location: {
+        lat: number,
+        lng: number
+    };
+    constructor(){
+        this.companyName = faker.company.companyName();
+        this.catchPrase = faker.company.catchPhrase();
+        this.location = {
+            lat: parseFloat(faker.address.latitude()),
+            lng: parseFloat(faker.address.longitude()),
+        };
+    }
+
+    markerContent():string{
+        return `
+        <h1>Company Name: ${this.companyName}</h1>
+        <h3>Catch Phrase: ${this.catchPrase}</h3>
+        `;
+    }
+}
